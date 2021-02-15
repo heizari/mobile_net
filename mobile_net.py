e6236ef86b3f5c556b6fae2727eb52413e6f243c
wsatoHMC <wataru.sato@hmcom.co.jp>
2021-02-15 11:35:04 +0900

    create mobilenet.py

---
+++ mobile_net.py
@@ -0,0 +1,9 @@
+import numpy as np
+import torch.nn as nn
+
+class MobileNet(nn.Module):
+    def __init__(self, channel_in, channel_out):
+        print('do init')
+    
+    def forward(self, x):
+        print('do forward')
